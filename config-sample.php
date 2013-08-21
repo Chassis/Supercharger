de5c1fad92adfa2b582211e5997e8101887061f5
Ryan McCue <me@ryanmccue.info>
2013-08-21 20:14:35 +1000

    Add sample configuration

---
+++ config-sample.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Custom configuration for this project
+ *
+ * Copy to config.php to use.
+ */
+
+// Set a default theme
+# defined('WP_DEFAULT_THEME') or define('WP_DEFAULT_THEME', 'collaborate');
